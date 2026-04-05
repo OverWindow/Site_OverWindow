3ff6d8475ce34c6c4f0d9009e4390f9c92cf717c
101511418+OverWindow@users.noreply.github.com <101511418+OverWindow@users.noreply.github.com>
2026-04-05 23:18:29 +0900

    hslk

--- frontend/src/api/links.js
+++ frontend/src/api/links.js
@@ -24,7 +24,7 @@ export async function getPublicLinks() {
   console.log('MODE =', import.meta.env.MODE);
   console.log('VITE_API_BASE_URL =', import.meta.env.VITE_API_BASE_URL);
   console.log('ALL_ENV =', import.meta.env);
-  console.log(API_BASE_URL)
+  console.log("hello",import.meta.env.API_BASE_URL)
   const response = await fetch(`${API_BASE_URL}/links/public`, {
     method: "GET",
     headers: {
